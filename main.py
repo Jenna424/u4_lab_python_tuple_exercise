4011e1a31492ebe9b1bfc7c764c7563d5791af27
Jenna Leopold <jleopold424@gmail.com>
2022-05-02 10:01:22 -0400

    completed Exercise 1

---
+++ main.py
@@ -0,0 +1,6 @@
+# Create a list named students containing some student names (strings)
+students = ['Jenna', 'Molly', 'Nghiem']
+# Print out the second student's name
+print(students[1])
+# Print out the last student's name
+print(students[-1])
