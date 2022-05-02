d60a2e8c07ed237dad28259e9140149a6a86fb3d
Jenna Leopold <jleopold424@gmail.com>
2022-05-02 10:07:02 -0400

    completed Exercise 2

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ students = ['Jenna', 'Molly', 'Nghiem']
 print(students[1])
 # Print out the last student's name
 print(students[-1])
+
+# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list
+foods = ('apple', 'banana', 'orange')
+# Use a for loop to print out the string "food goes here is a good food"
+for food in foods:
+    print('{} is a good food'.format(food))
